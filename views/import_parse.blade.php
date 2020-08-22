0926ce1a00ed521119e244f259d6fb9aee8b25d2
Chetan Khandla <ckhandla94@gmail.com>
2020-08-22 09:21:51 +0530

    fix ui and route issues

--- views/import_parse.blade.php
+++ views/import_parse.blade.php
@@ -1,65 +1,65 @@
-@extends('backpack::layout')
+@extends(backpack_view('blank'))
+
 
 @section('header')
-    <section class="content-header">
-        <h1>
-            <span class="text-capitalize">{!! $crud->getHeading() ?? $crud->entity_name_plural !!}</span>
-            <small>{!! $crud->getSubheading() ?? 'Import Mapping for '.$crud->entity_name !!}.</small>
-        </h1>
-        <ol class="breadcrumb">
-            <li>
-                <a href="{{ url(config('backpack.base.route_prefix'), 'dashboard') }}">{{ trans('backpack::crud.admin') }}</a>
-            </li>
-            <li><a href="{{ url($crud->route) }}" class="text-capitalize">{{ $crud->entity_name_plural }}</a></li>
-            <li class="active">Import Mapping</li>
-        </ol>
-    </section>
+  <div class="container-fluid">
+    <h2>
+      <span class="text-capitalize">Import Mapping for {!! $crud->getHeading() ?? $crud->entity_name_plural !!}</span>
+      <small id="datatable_info_stack">{!! $crud->getSubheading() ?? '' !!}</small>
+    </h2>
+  </div>
 @endsection
 
 @section('content')
 
-    <div class="container m-l-5 m-r-5" style="width: 100%;">
-        <div class="row">
-            <form class="form-horizontal" method="POST" action="{{ url($crud->route.'/import-process') }}">
-                {{ csrf_field() }}
-                <input type="hidden" name="csv_data_file_id" value="{{ $csv_data_file->id }}"/>
+    <div class="row mt-4">
+        <div class="card">
+            <div class="card-body">
+                <form class="form-horizontal" method="POST" action="{{ url($crud->route.'/import-process') }}">
+                    {{ csrf_field() }}
+                    <input type="hidden" name="csv_data_file_id" value="{{ $csv_data_file->id }}"/>
+                    <div class="mb-3"> 
+                        <a href="{{ url($crud->route.'/import') }}" class="btn btn-default">Back</a>     
+                        <button type="submit" class="btn ml-2 btn-primary">Import Data</button> 
+                    </div>
 
-                <div style="overflow: auto; width: 100%;">
-                    <table class="box table table-striped table-hover responsive nowrap m-t-0" style="width: 100%">
-                        @foreach ($csv_data as $row)
-                            <tr>
-                                @foreach ($row as $key => $value)
-                                    <td>{{ $value }}</td>
+                    <div class="responsive-table">
+                        <table class="table table-striped table-hover responsive nowrap w-100">
+                             <tr>
+                                @foreach ($csv_data[0] as $key => $value)
+                                    <td>
+                                        <select name="fields[{{ $key }}]" class="form-control select2">
+                                            <option value="">Select mapping</option>
+                                            @foreach ($import_fields as $key => $import_field)
+                                                <option
+                                                        @if ($import_field['name']===$value || $import_field['label']===$value)
+                                                        selected
+                                                        @endif
+                                                        value="{{ $import_field['name'] }}">{{ $import_field['label'] }}
+                                                </option>
+                                            @endforeach
+                                        </select>
+                                    </td>
                                 @endforeach
                             </tr>
-                        @endforeach
-                        <tr>
-                            @foreach ($csv_data[0] as $key => $value)
-                                <td>
-                                    <select name="fields[{{ $key }}]" class="form-control select2">
-                                        <option value="">Select mapping</option>
-                                        @foreach ($import_fields as $key => $import_field)
-                                            <option
-                                                    @if ($import_field['name']===$value || $import_field['label']===$value)
-                                                    selected
-                                                    @endif
-                                                    value="{{ $import_field['name'] }}">{{ $import_field['label'] }}
-                                            </option>
-                                        @endforeach
-                                    </select>
-                                </td>
-                            @endforeach
-                        </tr>
-                    </table>
-                </div>
 
-                <div style="float: left;">
-                    <a href="{{ route('admin.'.strtolower(str_replace(' ', '', $crud->entity_name)).'.import') }}" class="btn btn-primary">Back</a>
-                </div>
-                <div style="float: right;">
-                    <button type="submit" class="btn btn-primary">Import Data</button>
-                </div>
-            </form>
+                            @foreach ($csv_data as $row)
+                                <tr>
+                                    @foreach ($row as $key => $value)
+                                        <td>{{ $value }}</td>
+                                    @endforeach
+                                </tr>
+                            @endforeach
+                           
+                        </table>
+                    </div>
+
+                    <div> 
+                        <a href="{{ url($crud->route.'/import') }}" class="btn btn-default">Back</a>     
+                        <button type="submit" class="btn ml-2 btn-primary">Import Data</button> 
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 
@@ -67,47 +67,12 @@
 
 
 @section('after_styles')
-    <!-- DATA TABLES -->
-    <link href="{{ asset('vendor/adminlte/bower_components/select2/dist/css/select2.min.css') }}" rel="stylesheet"
-          type="text/css"/>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2-bootstrap-theme/0.1.0-beta.10/select2-bootstrap.min.css"
-          rel="stylesheet" type="text/css"/>
-
-    <style>
-        .select2-container .select2-selection--single {
-            box-sizing: border-box;
-            cursor: pointer;
-            display: block;
-            height: 35px;
-            user-select: none;
-            -webkit-user-select: none;
-        }
-
-        .select2-container {
-            width: auto !important;
-        }
-    </style>
-
-    <!-- CRUD LIST CONTENT - crud_list_styles stack -->
-    @stack('crud_fields_styles')
-@endsection
-
-@section('after_scripts')
-
-    <script src="{{ asset('vendor/adminlte/bower_components/select2/dist/js/select2.min.js') }}"></script>
-
     <style type="text/css">
-        .select2-selection__clear::after {
-            content: ' Clear';
+        table tr th,
+        table tr td {
+            max-width: 400px;
+            width: auto;
         }
     </style>
-
-    <script>
-        $(document).ready(function () {
-            $('select.select2').select2();
-        });
-    </script>
-
-    <!-- CRUD LIST CONTENT - crud_list_scripts stack -->
-    @stack('crud_fields_scripts')
 @endsection
+
--- views/import_process.blade.php
+++ views/import_process.blade.php
@@ -1,57 +1,53 @@
-@extends('backpack::layout')
+@extends(backpack_view('blank'))
+
 
 @section('header')
-    <section class="content-header">
-        <h1>
-            <span class="text-capitalize">{!! $crud->getHeading() ?? $crud->entity_name_plural !!}</span>
-            <small>{!! $crud->getSubheading() ?? 'Import Results for '.$crud->entity_name !!}.</small>
-        </h1>
-        <ol class="breadcrumb">
-            <li>
-                <a href="{{ url(config('backpack.base.route_prefix'), 'dashboard') }}">{{ trans('backpack::crud.admin') }}</a>
-            </li>
-            <li><a href="{{ url($crud->route) }}" class="text-capitalize">{{ $crud->entity_name_plural }}</a></li>
-            <li class="active">Import Result</li>
-        </ol>
-    </section>
+  <div class="container-fluid">
+    <h2>
+      <span class="text-capitalize">Import Results for {!! $crud->getHeading() ?? $crud->entity_name_plural !!}</span>
+      <small id="datatable_info_stack">{!! $crud->getSubheading() ?? '' !!}</small>
+    </h2>
+  </div>
 @endsection
 
 @section('content')
 
-    <div class="container m-l-5 m-r-5" style="width: 100%;">
-        <div class="row">
-            @if (empty($errors))
-                <h1 style="text-align: center; margin: 150px;">Imported successfully</h1>
-            @else
-                <div style="overflow: auto;">
-                    <table class="box table table-striped table-hover display responsive nowrap m-t-0 dataTable">
-                        <tr>
-                            <td>Row Number</td>
-                            <td>Error Messages</td>
-                        </tr>
-                        @foreach ($errors as $idx_1 => $error)
-                            <tr>
-                                <td>{{ $error['row_number'] }}</td>
-                                <td>
-                                    <ul style="padding-left: 15px;">
-                                        @foreach($error['errors'] as $idx_2 => $e)
-                                            <li>{{ $e }}</li>
-                                        @endforeach
-                                    </ul>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </table>
+    <div class="row mt-4">
+        <div class="card col-md-4">
+            <div class="card-body">
+                @if (empty($errors))
+                <div class="alert alert-success text-center">
+                    Imported successfully
                 </div>
-            @endif
+                @else
+                    <div>
+                        <table class="box table table-striped table-hover display responsive nowrap m-t-0 dataTable">
+                            <tr>
+                                <td>Row Number</td>
+                                <td>Error Messages</td>
+                            </tr>
+                            @foreach ($errors as $idx_1 => $error)
+                                <tr>
+                                    <td>{{ $error['row_number'] }}</td>
+                                    <td>
+                                        <ul style="padding-left: 15px;">
+                                            @foreach($error['errors'] as $idx_2 => $e)
+                                                <li>{{ $e }}</li>
+                                            @endforeach
+                                        </ul>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                @endif
 
-            <div style="float: left;">
-                <a href="{{ route('admin.'.strtolower(str_replace(' ', '', $crud->entity_name)).'.import') }}" class="btn btn-primary">Import More</a>
-            </div>
-            <div style="float: right;">
-                <a href="{{ url($crud->route) }}" class="btn btn-primary">Finished</a>
-            </div>
+                <div class="mt-3 text-center">
+                    <a href="{{ url($crud->route.'/import') }}" class="btn btn-primary">Import More</a>
 
+                    <a href="{{ url($crud->route) }}" class="btn ml-2 btn-success">Finished</a>
+                </div>
+            </div>
         </div>
     </div>
 
